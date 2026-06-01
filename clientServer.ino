6b655207e79c568a60e410879ee55f32005bccc3
Maroofalam <125355198+Maroofalam@users.noreply.github.com>
2026-06-01 10:18:17 +0530

    Delete clientServer.ino

--- clientServer.ino
+++
@@ -1,8 +0,0 @@
-// #include<DFPlayer_Mini_Mp3.h>
-// SoftwareSerial mp3Serial(2, 16);
-// mp3Serial.begin(9600);
-// mp3_set_serial(mp3Serial);
-// mp3_set_volume(30);
-// delay(500);
-// mp3_play(8821);
-// delay(5000);
